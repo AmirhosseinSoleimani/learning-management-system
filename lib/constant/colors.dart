ae23d54171a13042f58aa11f9cc92c7980b7512d
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-10-03 18:55:12 +0330

    introduction screen

---
+++ lib/constant/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF00004E);
+const secondary = Color(0xFF2D527C);
+const mainColor = Color(0xFF000000);
+const appBgColor = Color(0xFF1D4CDE);
+const appBarColor = Color(0xFF21D3EE);
+const bottomBarColor = Color(0xFFF9F9FB);
+const inActiveColor = Colors.grey;
+const shadowColor = Colors.black87;
+const textBoxColor = Colors.white;
+const textColor = Color(0xFF333333);
---
+++ lib/introScreen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(
+        globalBackgroundColor: Colors.white,
+        scrollPhysics: BouncingScrollPhysics(),
+        pages: [
+          PageViewModel(
+            titleWidget: const Text('Write Title of Page',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            body: 'Write the more description of the page.',
+            image: Image.network('https://cdn.elearningindustry.com/wp-content/uploads/2020/07/3-terms-everyone-in-digital-learning-should-know.png',
+              height: 400,
+              width: 400,
+            )
+          ),
+          PageViewModel(
+              titleWidget: const Text('Write Title of Page',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              body: 'Write the more description of the page.',
+              image: Image.network('https://cdn.elearningindustry.com/wp-content/uploads/2020/07/3-terms-everyone-in-digital-learning-should-know.png',
+                height: 400,
+                width: 400,
+              )
+          ),
+          PageViewModel(
+              titleWidget: const Text('Write Title of Page',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              body: 'Write the more description of the page.',
+              image: Image.network('https://cdn.elearningindustry.com/wp-content/uploads/2020/07/3-terms-everyone-in-digital-learning-should-know.png',
+                height: 400,
+                width: 400,
+              )
+          ),
+        ],
+        onDone: (){},
+        onSkip: (){},
+        showSkipButton: true,
+        skip: const Text(
+          'Skip',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+            color: Color(0xFF6C63FF)
+          ),
+        ),
+          next: const Icon(Icons.arrow_forward,
+            color: Color(0xFF6C63FF),
+          ),
+        done: const Text(
+        'Done',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+            color: Color(0xFF6C63FF)
+        ),
+      ),
+        dotsDecorator: DotsDecorator(
+          size: const Size.square(10.0),
+          activeSize: const Size(20.0,10.0),
+          color: Colors.black26,
+          activeColor: const Color(0xFF6C63FF),
+          spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0)
+          )
+        ),
+      ),
+    );
+  }
+}
