64b51740f89f1fef7bdc74b8945a2b17e5974e38
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-10-23 17:51:21 +0330

    add stepper to Signup Student favourite page

--- lib/authentication/sign_up/student/favourite.dart
+++ lib/authentication/sign_up/student/favourite.dart
@@ -39,7 +39,190 @@ class FavouriteStudent extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 35.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    child: Image.asset(
+                      'assets/images/epent_only_logo.png',
+                      width: 120.0,
+                      height: 120.0,
+                      alignment: Alignment.topLeft,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Color(0xff177FB0),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10.0),
+                            topLeft: Radius.circular(10.0),
+                        ),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              SizedBox(
+                                width: 25,
+                                height: 35,
+                                child: CircleAvatar(
+                                  backgroundColor: Color(0xff5DBF23),
+                                  child: Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                    size: 25.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                height: 35,
+                                child: MySeparator(
+                                  color: Color(0xffD9D9D9),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 25,
+                                height: 35,
+                                child: CircleAvatar(
+                                  backgroundColor: Color(0xff5DBF23),
+                                  child: Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                    size: 25.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                height: 35,
+                                child: MySeparator(
+                                  color: Color(0xffD9D9D9),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 25,
+                                height: 35,
+                                child: CircleAvatar(
+                                  backgroundColor: Color(0xff5DBF23),
+                                  child: Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                    size: 25.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                height: 35,
+                                child: MySeparator(
+                                  color: Color(0xffD9D9D9),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 25,
+                                height: 35,
+                                child: CircleAvatar(
+                                    backgroundColor: Color(0xff5DBF23),
+                                    child: null),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              Text(
+                                'Information',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                              SizedBox(
+                                width: 13.0,
+                              ),
+                              Text(
+                                'Information',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              Text(
+                                'Favourite',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Padding(
+                                padding: EdgeInsets.only(left: 252),
+                                child: SizedBox(
+                                  width: 60,
+                                  height: 1,
+                                  child: Divider(
+                                    thickness: 5,
+                                    color: Color(0xff5DBF23),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
