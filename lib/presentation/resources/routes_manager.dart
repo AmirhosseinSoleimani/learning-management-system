047176bb844f87310c175b9835f78719403da3bf
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-14 14:23:06 +0330

    Add RoutesManager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../presentation/resources/string_manager.dart';
+import '../../store/shop_page/screens/shop_page.dart';
+
+class Routes{
+  static const String shopPageRoute = '/shop';
+}
+
+
+class RouteGenerator{
+  static Route<dynamic> getRoute(RouteSettings routeSettings){
+    switch(routeSettings.name){
+      case Routes.shopPageRoute:
+        return MaterialPageRoute(builder: (_) => const ShopPage());
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+  static Route<dynamic> unDefinedRoute(){
+    return MaterialPageRoute(builder: (_) => Scaffold(
+      appBar: AppBar(
+        title: const Text(AppString.noRouteFound),
+      ),
+      body: const Center(
+        child: Text(AppString.noRouteFound),
+      ),
+    ),
+    );
+  }
+}
