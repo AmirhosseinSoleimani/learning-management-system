3c7891f46872e5e49ac3b0f1e0e83731fda7b9a3
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-20 11:28:11 +0330

    Add StoreModel

---
+++ lib/models/store_model.dart
@@ -0,0 +1,22 @@
+class StoreModel{
+  String id;
+  String name;
+  String image;
+  String price;
+  String duration;
+  String session;
+  String review;
+  String description;
+
+  StoreModel({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.duration,
+    required this.session,
+    required this.review,
+    required this.description
+
+  });
+}
