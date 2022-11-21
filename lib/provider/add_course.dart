72d5be0c295dde051798bebc7264552821a32e36
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-11-21 20:20:22 +0330

    Add addCourseProvider in TitlePage in addCourse

---
+++ lib/provider/add_course.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../models/add_course.dart';
+
+class AddCourseProvider with ChangeNotifier{
+  final List<LearnThingModel> _learnThings = [];
+
+  List<LearnThingModel> get learnThings{
+    return _learnThings;
+  }
+
+  void addLearnThingsList(LearnThingModel learnThingModel){
+    learnThings.add(learnThingModel);
+    notifyListeners();
+  }
+}
