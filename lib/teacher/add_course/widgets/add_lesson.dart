cda4159fe5a4b47a3eb4fed5a61aa0db3e8ee49a
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-12-11 13:20:14 +0330

    Add AddLesson to LessonPage

---
+++ lib/teacher/add_course/widgets/add_lesson.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:learning_management_system/provider/section_provider.dart';
+import 'package:provider/provider.dart';
+import '../../../presentation/resources/color_manager.dart';
+
+class AddLesson extends StatefulWidget {
+  const AddLesson({Key? key}) : super(key: key);
+
+  @override
+  State<AddLesson> createState() => _AddLessonState();
+}
+
+class _AddLessonState extends State<AddLesson> {
+  final FocusNode _titleFocusNode = FocusNode();
+  String? titleSection;
+  bool selectedSection = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Container(
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(
+                  width: 2,
+                  color: ColorManager.lightSteelBlue2
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+              color: ColorManager.lightSteelBlue5,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor:ColorManager.lightSteelBlue5,
+                border: const OutlineInputBorder(
+                    borderSide: BorderSide.none
+                ),
+                hintText: 'Title Section : ',
+                hintStyle: TextStyle(
+                  fontSize: 18,
+                  color:ColorManager.slateGray2,
+                  fontWeight: FontWeight.w400,
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              autofocus: true,
+              keyboardType: TextInputType.text,
+              focusNode: _titleFocusNode,
+              textInputAction: TextInputAction.done,
+              onChanged: (value){
+                setState(() {
+                  titleSection = value;
+                });
+              },
+              onFieldSubmitted: (value){
+                Provider.of<SectionProvider>(context,listen: false).addCourseTitle(context,titleSection!);
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          TextButton(
+              onPressed: () {
+                if(titleSection != null){
+                  Provider.of<SectionProvider>(context,listen: false).addCourseTitle(context,titleSection!);
+                  Navigator.of(context).pop();
+                }
+                else{
+                  setState(() {});
+                }
+              },
+              style: ButtonStyle(
+                  backgroundColor: (titleSection != null) ? MaterialStateProperty.all<Color>(
+                    ColorManager.primary,
+                  ) : MaterialStateProperty.all<Color>(
+                    ColorManager.lightSteelBlue2,
+                  ),
+                  shape:
+                  MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius:
+                          BorderRadius.circular(50.0)))),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: (titleSection != null) ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Add',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: ColorManager.white),
+                    ),
+                    const SizedBox(
+                      width: 5.0,
+                    ),
+                    Icon(
+                      Icons.done,
+                      color: ColorManager.white,
+                      size: 30,
+                    )
+                  ],
+                ):
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Add',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: ColorManager.lightSteelBlue1),
+                    ),
+                    const SizedBox(
+                      width: 5.0,
+                    ),
+                    Icon(
+                      Icons.done,
+                      color: ColorManager.lightSteelBlue1,
+                      size: 30,
+                    )
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/teacher/add_course/widgets/lessons.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../presentation/resources/assets_manager.dart';
+import '../../../presentation/resources/color_manager.dart';
+import '../../../presentation/resources/routes_manager.dart';
+import '../../../presentation/resources/values_manager.dart';
+import 'add_lesson.dart';
+import 'add_section.dart';
+
+class Lessons extends StatelessWidget {
+  const Lessons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: ColorManager.black,
+            size: AppSize.s20,
+          ),
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed(Routes.addCourseTitle);
+          },
+        ),
+        title: Image.asset(
+          ImageAssets.epent,
+          width: MediaQuery.of(context).size.width * 0.3,
+          height: MediaQuery.of(context).size.height * 0.08,
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: ColorManager.black, size: AppSize.s30),
+        actions: [
+          IconButton(onPressed: (){
+            showModalBottomSheet(
+                context: context, builder: (context) => const AddLesson());
+          }, icon: const Icon(Icons.add,size: 25.0,),
+          )
+        ],
+      ),
+    );
+  }
+}
