311cba38475452fa37754443475ee12760bc2e6d
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2022-12-11 12:46:14 +0330

    Add addBlogContent Structure

---
+++ lib/teacher/add_blog/screens/add_blog_content.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../../../presentation/resources/assets_manager.dart';
+import '../../../presentation/resources/color_manager.dart';
+import '../../../presentation/resources/routes_manager.dart';
+import '../../../presentation/resources/values_manager.dart';
+
+class AddBlogContent extends StatelessWidget {
+  const AddBlogContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: ColorManager.black,
+            size: AppSize.s20,
+          ),
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed(Routes.addBlogLayout);
+          },
+        ),
+        title: Image.asset(
+          ImageAssets.epent,
+          width: MediaQuery.of(context).size.width * 0.3,
+          height: MediaQuery.of(context).size.height * 0.08,
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: ColorManager.black, size: AppSize.s30),
+      ),
+    );
+  }
+}
